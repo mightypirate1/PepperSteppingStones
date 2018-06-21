15e192478a816661e0bfdd4ca8f2f558cc7e6486
Martin Frisk <yfflan@gmail.com>
2018-06-21 16:54:43 +0200

    Added PepperAgenFat!

--- Assets/Scenarios/PepperSocial/Scripts/PepperAgentSecond.cs
+++ Assets/Scenarios/PepperSocial/Scripts/PepperAgentSecond.cs
@@ -9,7 +9,7 @@ public class PepperAgentSecond : PepperAgentCommmon{
         // Vector3 offset = new Vector3(0.0f,1.0f,0.0f);
         Vector3 offset = 1f*rBody.transform.up + 0.1f*rBody.transform.forward;
         this.cam.transform.position = transform.position + offset;
-        this.cam.transform.forward = Quaternion.AngleAxis(30, rBody.transform.right) * rBody.transform.forward;
+        this.cam.transform.forward = Quaternion.AngleAxis(40, rBody.transform.right) * rBody.transform.forward;
     }
 
     public override void InitializeAgent()
